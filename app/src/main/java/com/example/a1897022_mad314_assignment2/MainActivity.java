20002970725ea3aa308b676f1c1cf10f873f21cb
sahil <latthersahil@gmail.com>
2019-10-19 12:38:15 -0400

    final chnage

--- app/src/main/java/com/example/a1897022_mad314_assignment2/MainActivity.java
+++ app/src/main/java/com/example/a1897022_mad314_assignment2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a1897022_mad314_assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,10 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
  String myusername="aman";
  String mypassword="12345";
+    RecyclerView rvStudents;
+    private StudentAdaptor mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvStudents = findViewById(R.id.rvStudents);
+        layoutManager = new LinearLayoutManager(this);
+        rvStudents.setLayoutManager(layoutManager);
+
+        mAdapter = new StudentAdapter(getApplicationContext());
+        rvStudents.setAdapter(mAdapter);
+
+        final StudentAdaptor = StudentAdaptor.getInstance();
 
         eTid=findViewById(R.id.eTid);
         eTpwd=findViewById(R.id.eTpwd);
